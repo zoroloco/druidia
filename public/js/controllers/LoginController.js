4da1cb47d399d02949902c5453bc83c9b8ee286f
Kenneth Centurion <kcenturion@yahoo.com>
2016-04-08 11:35:32 -0500

    logon

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -1,9 +1,11 @@
 angular.module('login-module', ['ngMaterial']).
   controller('LoginController',['$log','LoginService',function($log,loginService) {
     var self = this;
+    this.username = "";
+    this.password = "";
 
     self.onLogon = function(){
-      $log.log("User is attempting to login.");
+      $log.log("User "+self.username+" is attempting to login.");
       //loginService.processLogin();
     }
 
