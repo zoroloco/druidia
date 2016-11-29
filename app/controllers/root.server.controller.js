e30f4b17f195066a0cd81dd5ba35d1a7226632bb
Kenneth Centurion <kcenturion@yahoo.com>
2016-11-29 11:12:17 -0600

    mobile detect

--- app/controllers/root.server.controller.js
+++ app/controllers/root.server.controller.js
@@ -1,13 +1,23 @@
 var log            = require(pathUtil.join(__dirname,'../lib/logger.js')),
-    _              = require('underscore');
+    _              = require('underscore'),
+    MobileDetect   = require('mobile-detect');
 
   exports.renderRoot = function(req,res,next){
     log.info("Root page requested.");
-    //res.redirect('/views/index.html');
-    if(req.session.authenticated){
-      res.sendFile(pathUtil.join(__dirname,'../../public/views/secure/index.html'));
+
+    var md = new MobileDetect(req.headers['user-agent']);
+
+    if(md.is('iPhone')){
+      log.info("Iphone detected!");
+      res.send("you is on iphone!");
     }
     else{
-      res.sendFile(pathUtil.join(__dirname,'../../public/views/login.html'));
+      //res.redirect('/views/index.html');
+      if(req.session.authenticated){
+        res.sendFile(pathUtil.join(__dirname,'../../public/views/secure/index.html'));
+      }
+      else{
+        res.sendFile(pathUtil.join(__dirname,'../../public/views/login.html'));
+      }  
     }
   };
