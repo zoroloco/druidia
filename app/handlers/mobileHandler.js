c416a3d9cc4d8a3c1cd04411d53d806bb2984412
Kenneth Centurion <kcenturion@yahoo.com>
2016-11-30 21:45:29 -0600

    frfff

--- app/handlers/mobileHandler.js
+++ app/handlers/mobileHandler.js
@@ -10,7 +10,10 @@ exports.reRouteMobile = function(req,res,next){
     log.info("hostname="+req.hostname);
     log.info("req.url="+req.url);
     log.info("req.originalURL="+req.originalURL);
-    
+    log.info("req.vhost[0]="+req.vhost[0]);
+    log.info("req.vhost.length="+req.vhost.length);
+    log.info("req.vhost.hostname="+req.vhost.hostname);
+
     var mobilePath = "https://mobile."+req.hostname+req.url;
     log.info("Mobile device detected! Rerouting to: "+mobilePath);
     res.redirect(mobilePath);
