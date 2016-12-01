5cc10edc4e62aee019a31e89ce7c314942368a2a
Kenneth Centurion <kcenturion@yahoo.com>
2016-11-30 22:15:30 -0600

    dfsdf

--- app/handlers/mobileHandler.js
+++ app/handlers/mobileHandler.js
@@ -1,6 +1,5 @@
 //Determines if mobile.
 var log          = require(pathUtil.join(__dirname,'../lib/logger.js')),
-    underStr     = require('underscore.string'),
     conf         = require(pathUtil.join(__dirname,'../config/conf.json')),
     MobileDetect = require('mobile-detect');
 
@@ -10,15 +9,10 @@ exports.reRouteMobile = function(req,res,next){
 
   if(md.is('iPhone')){//TODO: add more mobile devices.
     log.info("Mobile device detected!");
-
-    if(underStr.startsWith(req.hostname,"www")){
-      req.hostname = underStr.replaceAll(req.hostname,"www",conf.virtualHostnameMobile);
-      log.info("Changed hostname to:"+req.hostname);
-      var mobilePath = "https://mobile."+req.hostname+req.url;
-      log.info("Rerouting to: "+mobilePath);
-      res.redirect(mobilePath);
-      return;
-    }
+    var mobilePath = "https://mobile."+conf.hostname+"/"+req.url;
+    log.info("Rerouting to: "+mobilePath);
+    res.redirect(mobilePath);
+    return;
   }
   else{
     next();//keep on proceeding to non mobile site request.
