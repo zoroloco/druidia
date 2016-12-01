8f58947d99874ed9018d75325ab67f0763ff6652
Kenneth Centurion <kcenturion@yahoo.com>
2016-11-30 22:08:14 -0600

    test

--- app/handlers/mobileHandler.js
+++ app/handlers/mobileHandler.js
@@ -9,17 +9,16 @@ exports.reRouteMobile = function(req,res,next){
   var md = new MobileDetect(req.headers['user-agent']);
 
   if(md.is('iPhone')){//TODO: add more mobile devices.
-    log.warn("hosname = "+req.hostname);
+    log.info("Mobile device detected!");
 
     if(underStr.startsWith(req.hostname,"www")){
-      req.hostname = underStr.replaceAll(req.hostname,"/www",conf.virtualHostnameMobile);
+      req.hostname = underStr.replaceAll(req.hostname,"www",conf.virtualHostnameMobile);
       log.info("Changed hostname to:"+req.hostname);
+      var mobilePath = "https://mobile."+req.hostname+req.url;
+      log.info("Rerouting to: "+mobilePath);
+      res.redirect(mobilePath);
+      return;
     }
-
-    var mobilePath = "https://mobile."+req.hostname+req.url;
-    log.info("Mobile device detected! Rerouting to: "+mobilePath);
-    res.redirect(mobilePath);
-    return;
   }
   else{
     next();//keep on proceeding to non mobile site request.
