dcd6e110966b15592f3a4dc34dd010fa0b85da14
Kenneth Centurion <kcenturion@yahoo.com>
2016-12-02 16:18:04 -0600

    fse

--- app/controllers/root.server.controller.js
+++ app/controllers/root.server.controller.js
@@ -14,5 +14,5 @@ var log            = require(pathUtil.join(__dirname,'../lib/logger.js')),
 
   exports.sendMobileRoot = function(req,res,next){
     log.info("Mobile root page requested.");
-    res.sendFile(pathUtil.join(__dirname,'../../public/mobile-views/secure/mobile-index.html'));
+    res.sendFile(pathUtil.join(__dirname,'../../public/views/secure/mobile-index.html'));
   }
