2c1649b30cf91a457f96f664d7206020b8113745
Kenneth Centurion <kcenturion@yahoo.com>
2016-12-28 14:38:20 -0600

    ignore dist folder

--- app-web/dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var canine_1 = require("./canine");
-var feline_1 = require("./feline");
-var garfield = new feline_1.Feline();
-var phoebe = new canine_1.Canine("Phoebe");
-phoebe.eat();
-garfield.drink();
