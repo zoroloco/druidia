1bf018ba0bca7337083edd63b84a32cda05fcd89
Kenneth Centurion <kcenturion@yahoo.com>
2017-06-03 16:36:19 -0500

    got rid of ng1 folder

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a MEAN stack version of druidia.net using:
+
+- Node JS 6+
+- Express 4+
+- Angular 2+
+- Typescript 2+
+- Mongo
--- app-js/public/js/app.js
+++
@@ -1,6 +0,0 @@
-// public/js/app.js
-angular.module('druidia-web', ['ngRoute',
-                               'route-module',
-                               'http-module',
-                               'index-module',
-                               'login-module']);
