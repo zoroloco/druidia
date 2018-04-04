2507a6221b614f67a1cc9277dffc060f626734fb
System Administrator <root@Kenneths-MBP.lan>
2018-04-04 10:04:19 -0500

    creds

---
+++ app-server/security/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+    cookieSecretValue: 'Lebrawn Jaymz test' ,
+    fb_app_id        : '1',
+    fb_app_secret    : '0',
+    jwtSecret        : 'tony romo never won the big game'
+};
